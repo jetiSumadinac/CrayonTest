d96ffab07b825cfd33a23fc38e0d9386862605c6
Stefan Vukovic <stefanvukovic991@gmail.com>
2019-05-16 20:35:31 +0200

    test controller is set and returning values
    at this point API works, took me an hour and a half to configure it

---
+++ CrayonTest/CrayonTest/Controllers/TestController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CrayonTest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+       
+        public TestController()
+        {
+        }
+
+        [HttpPost("test-post")]
+        public IActionResult Test()
+        {
+            return Ok("penis");
+        }
+        
+    }
+}
