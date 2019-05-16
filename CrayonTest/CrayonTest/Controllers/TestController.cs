0bd0c70fe1d795032836301d87acb96d8adf9e39
Stefan Vukovic <stefanvukovic991@gmail.com>
2019-05-16 21:07:36 +0200

    Http Client successfully returns a values from Exchange Rate API

--- CrayonTest/CrayonTest/Controllers/TestController.cs
+++ CrayonTest/CrayonTest/Controllers/TestController.cs
@@ -8,6 +8,11 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace CrayonTest.Controllers
 {
+    public class BaseCurrencies {
+        public string Base { get; set; }
+        public Dictionary<string, string> Rates { get; set; }
+        public DateTime Date { get; set; }
+    }
     [Route("api/[controller]")]
     [ApiController]
     public class TestController : ControllerBase
@@ -18,9 +23,19 @@ namespace CrayonTest.Controllers
         }
 
         [HttpPost("test-post")]
-        public IActionResult Test()
+        public async Task<IActionResult> Test()
         {
             return Ok("penis");
+        }
+
+        [HttpGet("test-http-client")]
+        public async Task<IActionResult> TestHttpClient()
+        {
+            HttpClient httpClient = new HttpClient();
+            var response = await httpClient.GetAsync("https://api.exchangeratesapi.io/latest"); //TODO: URL should be localized
+            var result = await response.Content.ReadAsAsync<BaseCurrencies>();
+
+            return Ok(result);
         }
         
     }
