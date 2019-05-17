18cf44f7012670eed31f0714b305d4156d01ba33
Stefan Vukovic <stefanvukovic991@gmail.com>
2019-05-17 02:19:24 +0200

    added TODO reminder

--- CrayonTest/CrayonTest/Models/OutputModels/ResponseRatesModel.cs
+++ CrayonTest/CrayonTest/Models/OutputModels/ResponseRatesModel.cs
@@ -8,7 +8,7 @@ namespace CrayonTest.Models.OutputModels
     public class ResponseRatesModel
     {
         public string Base { get; set; }
-        public List<Dictionary<DateTime, List<Dictionary<string, double>>>> Rates { get; set; }
+        public List<Dictionary<DateTime, List<Dictionary<string, double>>>> Rates { get; set; } //TODO: Nooo way mate
         //public int MyProperty { get; set; }
 
     }
