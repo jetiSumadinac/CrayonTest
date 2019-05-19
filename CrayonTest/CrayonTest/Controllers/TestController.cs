dae5e4bd9bb5abb0de7cdf8df321b60c6fab6854
Stefan Vukovic <stefanvukovic991@gmail.com>
2019-05-19 23:37:42 +0200

    testing code deleted

--- CrayonTest/CrayonTest/Controllers/TestController.cs
+++ CrayonTest/CrayonTest/Controllers/TestController.cs
@@ -12,12 +12,6 @@ using Newtonsoft.Json;
 
 namespace CrayonTest.Controllers
 {
-    public class BaseCurrencies
-    {
-        public string Base { get; set; }
-        public Dictionary<string, string> Rates { get; set; }
-        public DateTime Date { get; set; }
-    }
     [Route("api/[controller]")]
     [ApiController]
     public class TestController : ControllerBase
@@ -28,35 +22,11 @@ namespace CrayonTest.Controllers
             _ratesService = ratesService;
         }
 
-        [HttpPost("test-post")]
-        public async Task<IActionResult> Test()
-        {
-            return Ok("Helooo from test!");
-        }
-
-        [HttpGet("test-http-client")]
-        public async Task<IActionResult> TestHttpClient()
-        {
-            HttpClient httpClient = new HttpClient();
-            var response = await httpClient.GetAsync("https://api.exchangeratesapi.io/latest"); //TODO: URL should be localized
-            var result = await response.Content.ReadAsAsync<BaseCurrencies>();
-
-            return Ok(result);
-        }
-
         [HttpGet("proof-of-concept")]
         public async Task<IActionResult> ProofOfConcept([FromBody]InputExchangeDataModel data)
         {
             var result = await _ratesService.GetRates(data);
             return Ok(result);
         }
-
-        #region private methods
-        static List<DateTime> SortDatesAscending(List<DateTime> list)
-        {
-            list.Sort((a, b) => a.CompareTo(b));
-            return list;
-        }
-        #endregion
     }
 }
